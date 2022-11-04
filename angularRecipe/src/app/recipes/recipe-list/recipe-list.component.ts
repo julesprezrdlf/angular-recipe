c739385bf298ac4bcbb7f8c5047a7da221f5e7ea
julesprezrdlf <julioprezrdlf@gmail.com>
2022-11-04 14:26:36 +0100

    terminando

--- angularRecipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angularRecipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+    recipes: Recipe[] = [
+    new Recipe ('A test','another test','https://cdn.shopify.com/s/files/1/0272/0202/7618/t/51/assets/lol-big-logo.svg?v=168172767227458556691663134004')
+    ];
   constructor() { }
 
   ngOnInit(): void {
